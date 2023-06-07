95e6d9e292b087ef78fe23019c1b3a731876549a
Ankur Sharma <ankursharma1493@gmail.com>
2023-06-07 09:47:57 +0530

    completed v1

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ const SUBMISSION = [
 
 ]
 
+const admin = [];
+
 
 
 app.post('/signup', function(req, res) {
@@ -38,41 +40,64 @@ app.post('/signup', function(req, res) {
   console.log(USERS);
   
   // return back 200 status code to the client
-  res.status(200).json("user signed up")
+  res.status(200).json({
+    status: "success",
+    message: "user signed up"
+  })
 })
 
 app.post('/login', function(req, res) {
   // Add logic to decode body
   // body should have email and password
-
+  const {email,password} = req.body;
+  const user = {
+    email,
+    password
+  }
   // Check if the user with the given email exists in the USERS array
   // Also ensure that the password is the same
-
-
   // If the password is the same, return back 200 status code to the client
   // Also send back a token (any random string will do for now)
+  USERS.filter((user)=>{
+    if(user.email === email && user.password === password){
+      return res.status(200).json({
+        status: "success",
+        message: "you have been logged in"
+      })
+    }
+  })
+
   // If the password is not the same, return back 401 status code to the client
-
-
-  res.send('Hello World from route 2!')
+  res.status(401).json('enter correct email and password')
 })
 
 app.get('/questions', function(req, res) {
 
   //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
+    res.status(200).json({
+    status: "success",
+    QUESTIONS
+  })
 })
 
 app.get("/submissions", function(req, res) {
    // return the users submissions for this problem
-  res.send("Hello World from route 4!")
+   res.status(200).json({
+    status: "success",
+    SUBMISSION
+  })
 });
 
 
 app.post("/submissions", function(req, res) {
    // let the user submit a problem, randomly accept or reject the solution
+   const submission = req.body.submission;
    // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
+   SUBMISSION.push(submission);
+  res.status(200).json({
+    status: "success",
+    submission
+  })
 });
 
 // leaving as hard todos
